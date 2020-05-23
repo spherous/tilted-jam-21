781de1635963868b4ff95a6e99cb6af94fb5a395
kirkpe <kirkperkins99@gmail.com>
2020-05-23 14:01:04 -0700

    Added GameManager and coinScript

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int CoinValue = 1;
+    public int TotalCoins;
+    public int CurrentAmountCoins;
+
+
     void OnTriggerEnter(Collider other)
     {
-       
+        if (gameObject.GetComponent<Pirate>()!=null)
+        {
+            CurrentAmountCoins = TotalCoins++;
+            PlayerPrefs.SetInt("Coins", CurrentAmountCoins);
+            Destroy(this.gameObject);
+        }
     }
 
 
@@ -19,7 +30,7 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TotalCoins = PlayerPrefs.GetInt("Coins", 0);
     }
 
     
