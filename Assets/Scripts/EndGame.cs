f8f5ec5bf13e3829715dd4bfadaad7958e53502b
Avea <aveakrause@gmail.com>
2020-05-23 21:18:31 -0700

    added end game

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] private Button EndButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndButton.onClick.AddListener(End);
+    }
+    private void End()
+    {
+        Application.Quit();
+    }
+}
