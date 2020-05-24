bc20def71256a5706ea8f7dc698b9b277c6e5767
kirkpe <kirkperkins99@gmail.com>
2020-05-23 19:45:04 -0700

    FixedNullReference

--- Assets/Scripts/CloudSpawn.cs
+++ Assets/Scripts/CloudSpawn.cs
@@ -7,24 +7,7 @@ public class CloudSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LookAlive lookAlive = this.GetComponent<LookAlive>();
-
-        float rot_freq = 0.05f;
-
-        lookAlive.rot_freq.y = Random.value * rot_freq;
-
-        float pos_freq = 0.05f;
-
-        lookAlive.pos_freq.x = Random.value * pos_freq;
-        lookAlive.pos_freq.y = Random.value * pos_freq;
-        //lookAlive.pos_freq.z = Random.value * pos_freq;
-
-        float amplitude = 5.0f;
-        float amplitude_min = 0;
-
-        lookAlive.pos_amplitude.x = (Random.value * amplitude) + amplitude_min;
-        lookAlive.pos_amplitude.y = (Random.value * amplitude) + amplitude_min;
-        //lookAlive.pos_amplitude.z = (Random.value * amplitude) + amplitude_min;
+       
 
     }
 
